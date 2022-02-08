07efc9407145f3a835a7f92e85ee8440420ba8fe
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-02-08 23:48:25 +0300

    Identity API ile bir üyelik sistemi oluşturmak-2

--- BaseProject/Models/AppIdentityDbContext.cs
+++ BaseProject/Models/AppIdentityDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BaseProject.Models
 {
-    public class AppIdentityDbContext : IdentityDbContext
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
     {
         public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
         {
--- BaseProject/Program.cs
+++ BaseProject/Program.cs
@@ -1,5 +1,10 @@
+using BaseProject.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Linq;
 
 namespace BaseProject
 {
@@ -7,7 +12,27 @@ namespace BaseProject
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+
+            var identityDbContext = scope.ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+            identityDbContext.Database.Migrate();
+
+            if (!userManager.Users.Any())
+            {
+                userManager.CreateAsync(new AppUser() { UserName = "User1", Email = "user1@outlook.com" }, "Password12*").Wait();
+                userManager.CreateAsync(new AppUser() { UserName = "User2", Email = "user2@outlook.com" }, "Password12*").Wait();
+                userManager.CreateAsync(new AppUser() { UserName = "User3", Email = "user3@outlook.com" }, "Password12*").Wait();
+                userManager.CreateAsync(new AppUser() { UserName = "User4", Email = "user4@outlook.com" }, "Password12*").Wait();
+                userManager.CreateAsync(new AppUser() { UserName = "User5", Email = "user5@outlook.com" }, "Password12*").Wait();
+
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
